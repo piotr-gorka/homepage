3d4d7d3311f0fe6161530047d7eddf9fd0f15d3c
Piotr <piotr-gorka@wp.pl>
2020-05-06 08:44:09 +0100

    first commit in homepage project

---
+++ main.js
@@ -0,0 +1,7 @@
+
+console.log('Dzień dobry!');
+const name = 'Piotr';
+const surname = 'Górka';
+const age = 23;
+
+console.log(`Nazywam się ${name} ${surname} i mam ${age} lata, a to jest mój pierwszy projekt WTF`);
