2b0fca90df84f091fe6f611050a1e9fe23463763
Piotr <piotr-gorka@wp.pl>
2020-05-12 12:50:06 +0100

    format document by prettier

--- js/main.js
+++ js/main.js
@@ -1,27 +1,25 @@
-
-const info = document.querySelector('.section__paragraph--js');
+const info = document.querySelector(".section__paragraph--js");
 console.log(info);
 
-info.innerHTML = 'Ten paragraf został napisany za pomocą JavaScript';
-
-
-
+info.innerHTML = "Ten paragraf został napisany za pomocą JavaScript";
 
 let person1 = {
-    name: 'Justyna',
-    age: 31,
-    drivingLicense: true
+  name: "Justyna",
+  age: 31,
+  drivingLicense: true,
 };
 
 let person2 = {
-    name:'Piotr',
-    age:48,
-    drivingLicense:true
+  name: "Piotr",
+  age: 48,
+  drivingLicense: true,
 };
 
-function greet (name, age){
-    return console.log(`Witaj ${name} , patrząc na Ciebie podejrzewam że masz ${age} lat :) `);
+function greet(name, age) {
+  return console.log(
+    `Witaj ${name} , patrząc na Ciebie podejrzewam że masz ${age} lat :) `
+  );
 }
 
-greet(person1.name,person1.age);
-greet(person2.name,person2.age);
+greet(person1.name, person1.age);
+greet(person2.name, person2.age);
